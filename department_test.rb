87d460f61cce223de5acc6570ccc6b92fccc21a8
MichaelByrd <michaelbyrdsemail@gmail.com>
2015-01-26 13:16:43 -0500

    create file structure

---
+++ department_test.rb
@@ -0,0 +1,7 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './department'
+
+class DepartmentTest < MiniTest::Test
+
+end
---
+++ employee_test.rb
@@ -0,0 +1,7 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './employee'
+
+class EmployeeTest < MiniTest::Test
+
+end
